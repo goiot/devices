45d140b7fb6a6f5b84ee4f6800e7b10d863fcb73
Matt Aimonetti <mattaimonetti@gmail.com>
2016-05-30 13:13:53 -0700

    switch the example test to the main package so it properly shows in godoc

--- i2c/grove/oled96x96/oled96x96_test.go
+++ i2c/grove/oled96x96/oled96x96_test.go
@@ -1,15 +1,13 @@
-package oled96x96_test
-
-import "github.com/goiot/drivers/i2c/grove/oled96x96"
+package oled96x96
 
 func ExampleHelloWorld() {
 	bus := &i2c.Devfs{
 		// change the following value if you use another bus
 		Dev:  "/dev/i2c-1",
-		Addr: oled96x96.Address,
+		Addr: Address,
 	}
 
-	display, err := oled96x96.New(bus)
+	display, err := New(bus)
 	if err != nil {
 		panic(err)
 	}
