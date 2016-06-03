e6d484453623a3075e2c4c6ce1452acf477e5856
Jaana Burcu Dogan <jbd@google.com>
2016-06-03 11:43:21 -0700

    dotstar: write the values to the bus

--- dotstar/dotstar.go
+++ dotstar/dotstar.go
@@ -7,16 +7,19 @@ import (
 )
 
 type RGBA struct {
-	R int
-	G int
-	B int
-	A int
+	R byte
+	G byte
+	B byte
+	A byte
 }
 
 type Device struct {
 	Device *spi.Device
 
 	vals []RGBA
+
+	beginFrame []byte
+	endFrame   []byte
 }
 
 func New(o driver.Opener, n int) (*Device, error) {
@@ -24,9 +27,15 @@ func New(o driver.Opener, n int) (*Device, error) {
 	if err != nil {
 		return nil, err
 	}
+	endFrame := make([]byte, 4*n/2)
+	for i := 0; i < len(endFrame); i++ {
+		endFrame[i] = 0xff
+	}
 	return &Device{
-		Device: dev,
-		vals:   make([]RGBA, n),
+		Device:     dev,
+		vals:       make([]RGBA, n),
+		beginFrame: []byte{0x00, 0x00, 0x00, 0x00},
+		endFrame:   endFrame,
 	}, nil
 }
 
@@ -35,7 +44,28 @@ func (d *Device) SetRGBA(i int, v RGBA) {
 }
 
 func (d *Device) Display() error {
-	panic("not yet implemented")
+	// TODO(jbd): dotstar allows other RGBA allignments, support those layouts.
+	n := len(d.vals)
+	tx := make([]byte, 1+(4*n)+(n/2))
+	tx[0] = 0x00
+	tx[1] = 0x00
+	tx[2] = 0x00
+	tx[3] = 0x00
+
+	for i, c := range d.vals {
+		j := (i + 1) * 4
+		tx[j] = c.A
+		tx[j+1] = c.B
+		tx[j+2] = c.G
+		tx[j+3] = c.R
+	}
+
+	// end frame with at least n/2 0xff vals
+	for i := 4*n + 1; i < len(tx); i++ {
+		tx[i] = 0xff
+	}
+
+	return d.Device.Tx(tx, nil)
 }
 
 func (d *Device) Close() error {
